4d4625d306e62b1d45e0f5be2f09c1875bec62ac
Jokin <jokinaizpuru28@gmail.com>
2023-01-18 19:19:55 +0100

    Deleted Pointers in Iterators

--- lexer.c
+++ lexer.c
@@ -6,87 +6,87 @@
 /*   By: jaizpuru <jaizpuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/12 15:16:59 by jaizpuru          #+#    #+#             */
-/*   Updated: 2023/01/18 17:03:35 by jaizpuru         ###   ########.fr       */
+/*   Updated: 2023/01/18 19:06:10 by jaizpuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*clean_words(t_cmd	*cmd, char	*prompt, int	*pos)
+char	*clean_words(t_cmd	*cmd, char	*prompt, int	pos)
 {
 	char *tmp;
 	int	start;
 	int	aux;
 
-	aux = (*pos);
-	start = (*pos);
+	aux = (pos);
+	start = (pos);
+	while (prompt[aux + 1] == ' ')
+                aux += 1;
 	i_cwords(prompt, &aux);
 	cmd->args[cmd->size++] = ft_substr(prompt, start, aux - start + 1);
 	tmp = ft_substr(prompt, aux, ft_strlen(prompt) - aux + 1);
 	if(prompt[aux + 1] != ' ' && prompt[aux + 1])
-		*pos = 0;
+		pos = 0;
 	else
-		*pos = 1;
+		pos = 1;
 	prompt = ft_strdup(tmp);
 	free(tmp);
-	free(prompt);
 	return (prompt);
 }
 
-char	*quotes_lexer(t_cmd	*cmd, char	*prompt, int	*pos)
+char	*quotes_lexer(t_cmd	*cmd, char	*prompt, int	pos)
 {
 	char *tmp;
 	int	start;
 	int	aux;
 
-	aux = (*pos);
-	start = (*pos);
+	aux = (pos);
+	start = (pos);
 	i_qwords(prompt, &aux);
 	cmd->args[cmd->size++] = ft_substr(prompt, start, aux - start + 1);
 	tmp = ft_substr(prompt, aux, ft_strlen(prompt) - aux + 1);
 	if(prompt[aux + 1] != ' ' && prompt[aux + 1])
-		*pos = 0;
+		pos = 0;
 	else
-		*pos = 1;
+		pos = 1;
 	prompt = ft_strdup(tmp);
 	free(tmp);
 	free(prompt);
 	return (prompt);
 }
 
-char	*double_quotes_lexer(t_cmd	*cmd, char	*prompt, int	*pos)
+char	*double_quotes_lexer(t_cmd	*cmd, char	*prompt, int	pos)
 {
 	int	start;
 	int	aux;
 	char *tmp;
 
-	aux = (*pos);
-	start = (*pos);
+	aux = (pos);
+	start = (pos);
 	i_dqwords(prompt, &aux);
 	cmd->args[cmd->size++] = ft_substr(prompt, start, aux - start + 1);
 	tmp = ft_substr(prompt, aux, ft_strlen(prompt) - aux + 1);
 	if(prompt[aux + 1] != ' ' && prompt[aux + 1])
-		*pos = 0;
+		pos = 0;
 	else
-		*pos = 1;
+		pos = 1;
 	prompt = ft_strdup(tmp);
 	free(tmp);
-	free(prompt);
 	return (prompt);
 }
 
-char	*pipes_lexer(t_cmd	*cmd, char	*prompt, int	*pos)
+char	*pipes_lexer(t_cmd	*cmd, char	*prompt, int	pos)
 {
 	int		aux;
 	char 	*tmp;
 
-	aux = (*pos);
+	aux = (pos);
 	cmd->args[cmd->size++] = ft_substr(prompt, aux, 1);
 	tmp = ft_substr(prompt, aux, ft_strlen(prompt) - aux + 1);
 	if(prompt[aux + 1] != ' ' && prompt[aux + 1])
-		*pos = 0;
+		pos = 0;
 	else
-		*pos = 1;
+		pos = 1;
 	prompt = ft_strdup(tmp);
 	free(tmp);
 	return (prompt);
--- lexer2.c
+++ lexer2.c
@@ -6,40 +6,40 @@
 /*   By: jaizpuru <jaizpuru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/17 12:18:09 by jaizpuru          #+#    #+#             */
-/*   Updated: 2023/01/18 12:31:00 by jaizpuru         ###   ########.fr       */
+/*   Updated: 2023/01/18 19:04:48 by jaizpuru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*in_lexer(t_cmd	*cmd, char	*prompt, int	*pos)
+char	*in_lexer(t_cmd	*cmd, char	*prompt, int	pos)
 {
 	int		aux;
 	char 	*tmp;
 
-	aux = (*pos);
+	aux = (pos);
 	cmd->args[cmd->size++] = ft_substr(prompt, aux, 1);
 	tmp = ft_substr(prompt, aux, ft_strlen(prompt) - aux + 1);
 	if(prompt[aux + 1] != ' ' && prompt[aux + 1])
-		*pos = 0;
+		pos = 0;
 	else
-		*pos = 1;
+		pos = 1;
 	prompt = ft_strdup(tmp);
 	free(tmp);
 	return (prompt);
 }
-char	*out_lexer(t_cmd	*cmd, char	*prompt, int	*pos)
+char	*out_lexer(t_cmd	*cmd, char	*prompt, int	pos)
 {
 	int		aux;
 	char 	*tmp;
 
-	aux = (*pos);
+	aux = (pos);
 	cmd->args[cmd->size++] = ft_substr(prompt, aux, 1);
 	tmp = ft_substr(prompt, aux, ft_strlen(prompt) - aux + 1);
 	if(prompt[aux + 1] != ' ' && prompt[aux + 1])
-		*pos = 0;
+		pos = 0;
 	else
-		*pos = 1;
+		pos = 1;
 	prompt = ft_strdup(tmp);
 	free(tmp);
 	return (prompt);
